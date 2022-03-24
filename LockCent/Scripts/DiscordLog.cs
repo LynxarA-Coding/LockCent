2e8912f8aaae261c8f9b947cf220585d1af14020
LynxarA <danilok02@yandex.ru>
2022-03-24 22:29:21 +0100

    Update DiscordLog.cs

--- LockCent/Scripts/DiscordLog.cs
+++ LockCent/Scripts/DiscordLog.cs
@@ -9,25 +9,39 @@ using Newtonsoft.Json;
 
 namespace LockCent.Scripts
 {
+    /*
+     LockCent @2022
+     by LynxarA
+    */
     class DiscordLog
     {
+        // Function that sends Discord Logs about an App
         public void SendLogUserLog(string type, string data_username, string data)
         {
+            // Token stored for Discord Webhook
             string token = "https://discord.com/api/webhooks/956218527688327208/yKrxzSymFaVPYWphGCKzpfrBj34bIoYDVaD__F24LG24I6ZbODrYynkKFe_b4Ndbm7Mq";
 
+            // Discord Bot Username (sender name)
             string _bot_username = "LockCent Logger";
 
+            // Creating a new HTTP Request
             WebRequest wr = (HttpWebRequest)WebRequest.Create(token);
 
+            // Request content type
             wr.ContentType = "application/json";
 
+            // Request Method
             wr.Method = "POST";
+
+            // Creating an empty json
             string json;
 
             using (var sw = new StreamWriter(wr.GetRequestStream()))
             {
+                // If log type is LogIn
                 if (type == "login")
                 {
+                    // Embed constructor
                     json = JsonConvert.SerializeObject(new
                     {
                         username = _bot_username,
@@ -44,8 +58,9 @@ namespace LockCent.Scripts
 
                     sw.Write(json);
                 }
-                else if (type == "logout")
+                else if (type == "logout") // IF Log Type is LogOut
                 {
+                    // Embed constructor
                     json = JsonConvert.SerializeObject(new
                     {
                         username = _bot_username,
@@ -62,8 +77,9 @@ namespace LockCent.Scripts
 
                     sw.Write(json);
                 }
-                else if (type == "setting")
+                else if (type == "setting") // If Log Type is Setting
                 {
+                    // Embed constructor
                     json = JsonConvert.SerializeObject(new
                     {
                         username = _bot_username,
@@ -81,7 +97,11 @@ namespace LockCent.Scripts
                     sw.Write(json);
                 }
             }
+
+            // Saving a response in case of an error
             var response = (HttpWebResponse)wr.GetResponse();
+
+            // Aborting a request (request ended)
             wr.Abort();
         }
     }
